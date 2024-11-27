b0018e6512637f332eaa41af268158aee867b4ee
Raluca Mercas <raluca.mercas02@gmail.com>
2024-11-27 14:41:23 +0200

    Clean up redundant directories and reinitialize project structure

--- lost_pet_app/settings.py
+++ lost_pet_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x7ci_+h02_a-zu$vn7bv(1)d7fx#n+f=31qecioov+5vv3dmga'
+SECRET_KEY = 'django-insecure-uzvm=6=lsyrepxo(5ed8_=b1gm)p^q#5nro)-7sa$i58&p9^fc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
