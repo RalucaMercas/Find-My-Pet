c373658de509b9cffd598a9c9d85edfc91ed1091
Raluca Mercas <raluca.mercas02@gmail.com>
2024-11-27 23:16:45 +0200

    custom User model with roles, migrations, and test cases added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Roles(models.TextChoices):
+        NORMAL_USER = 'Normal'
+        ADMIN = 'Admin'
+        SUPERADMIN = 'SuperAdmin'
+
+    role = models.CharField(
+        max_length=20,
+        choices=Roles.choices,
+        default=Roles.NORMAL_USER,
+    )
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name="custom_user_set",
+        blank=True,
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="custom_user_permissions_set",
+        blank=True,
+    )
+
+    def save(self, *args, **kwargs):
+        if self.role == self.Roles.SUPERADMIN:
+            self.is_superuser = True
+            self.is_staff = True
+        elif self.role == self.Roles.ADMIN:
+            self.is_superuser = False
+            self.is_staff = True
+        else:
+            self.is_superuser = False
+            self.is_staff = False
+        super().save(*args, **kwargs)
+
+    def is_admin(self):
+        return self.role == self.Roles.ADMIN
+
+    def is_superadmin(self):
+        return self.role == self.Roles.SUPERADMIN
+
+    def is_normal_user(self):
+        return self.role == self.Roles.NORMAL_USER
--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+
+class UserModelTest(TestCase):
+    def test_user_creation(self):
+        user = User.objects.create(username="dorel", role=User.Roles.NORMAL_USER)
+        self.assertEqual(user.username, "dorel")
+        self.assertEqual(user.role, User.Roles.NORMAL_USER)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+        admin_user = User.objects.create(username="maricica", role=User.Roles.ADMIN)
+        self.assertEqual(admin_user.username, "maricica")
+        self.assertEqual(admin_user.role, User.Roles.ADMIN)
+        self.assertTrue(admin_user.is_staff)
+        self.assertFalse(admin_user.is_superuser)
+
+        superadmin_user = User.objects.create(username="lenutza", role=User.Roles.SUPERADMIN)
+        self.assertEqual(superadmin_user.username, "lenutza")
+        self.assertEqual(superadmin_user.role, User.Roles.SUPERADMIN)
+        self.assertTrue(superadmin_user.is_staff)
+        self.assertTrue(superadmin_user.is_superuser)
+
+        users = User.objects.all()
+        for user in users:
+            print(user.username, user.role, "Is staff:", user.is_staff, "Is superuser:", user.is_superuser)
