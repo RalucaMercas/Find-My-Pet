4775a3cfb6b83007a552d9ba541e85d56775eb91
Raluca Mercas <raluca.mercas02@gmail.com>
2024-12-04 17:44:30 +0200

    customized Change Password page

--- main/urls.py
+++ main/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('home', home, name='home'),
     path('sign-up', sign_up, name='sign_up'),
     path('log-out', log_out, name='log_out'),
-    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='admin/password_change_form.html'),
+         name='password_change'),
+
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
